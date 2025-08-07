5ef895545bade130cb22ae2e8041e3a5333dec49
UndineA <87313316+UndineA@users.noreply.github.com>
2025-08-07 13:08:22 +0000

    Quiz questions, rows

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,11 @@
 class Quiz < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
 
     has_many :questions, dependent: :destroy
     accepts_nested_attributes_for :questions, allow_destroy: true
+
+    def to_s
+        title
+    end
 end
